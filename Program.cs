1572c4d6f3e247859d35ba8bde258c9a8197cec6
AfruzCoder <husanovafruz1@gmail.com>
2024-03-07 10:59:06 +0500

    Update Program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int number ,number_2;
+System.Console.Write("Enter first number: ");
+number = int.Parse(Console.ReadLine());
+System.Console.Write("Enter first number: ");
+number_2 = int.Parse(Console.ReadLine());
+string amal;
+System.Console.Write("amalni kiriting: ");
+amal = Console.ReadLine();
+number = ((int)number);
+
+
+if (amal == "+")
+{
+    System.Console.WriteLine(number + number_2);
+}
+else if (amal == "-")
+{
+    System.Console.WriteLine(number - number_2);
+}
+else if (amal == "*")
+{
+    System.Console.WriteLine(number * number_2);
+}
+else if (amal == "/")
+{
+    System.Console.WriteLine(number / number_2);
+}
